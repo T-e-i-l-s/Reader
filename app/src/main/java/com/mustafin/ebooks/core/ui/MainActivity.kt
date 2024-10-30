d50e8fb91b5d57b10d7693b48eada54f718dc603
Karim <kfmust@gmail.com>
2024-10-30 22:02:23 +0300

    Keep Screen On Feature

--- app/src/main/java/com/mustafin/ebooks/core/ui/MainActivity.kt
+++ app/src/main/java/com/mustafin/ebooks/core/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mustafin.ebooks.core.ui
 import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -22,9 +23,18 @@ class MainActivity : ComponentActivity() {
         */
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
 
+        // Запрещаем экрану гаснуть
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
         enableEdgeToEdge()
         setContent {
             NavigationGraph()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Снимаем флаг при завершении активности, чтобы экран мог гаснуть
+        window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 }
